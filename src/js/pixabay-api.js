9546c9c73f48986d110a9f13d79c77425824dda1
Oleksandra Zakharova <oleksandraozakharova@gmail.com>
2024-12-16 03:53:35 +0100

    1

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,32 +2,42 @@ import axios from 'axios';
 
 const API_KEY = '47411566-1fe68ca00fdccc973e940a4de';
 
-export async function fetchImages(value, page) {
-  const params = new URLSearchParams({
-    key: API_KEY,
-    q: value,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    per_page: 15,
-  });
 
-  const response = await axios.get(`https://pixabay.com/api/?${params}`);
-  return response.data;
+export async function fetchImages(value, page) {
+  try {
+    const params = new URLSearchParams({
+      key: API_KEY,
+      q: value,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 15,
+    });
+
+    const response = await axios.get(`https://pixabay.com/api/?${params}`);
+    return response.data; 
+  } catch (error) {
+    console.error('Error fetching images:', error); 
+    throw error; 
+  }
 }
 
+
 export async function fetchNextPage(value, page) {
-  const params = new URLSearchParams({
-    key: API_KEY,
-    q: value,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    per_page: 15,
-    page,
-  });
+  try {
+    const params = new URLSearchParams({
+      key: API_KEY,
+      q: value,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 15,
+      page,
+    });
 
-  const response = await axios.get(`https://pixabay.com/api/?${params}`);
-
-  return response.data;
+    const response = await axios.get(`https://pixabay.com/api/?${params}`);
+    return response.data; 
+    console.error('Error fetching next page:', error); 
+    throw error; 
+  }
 }
